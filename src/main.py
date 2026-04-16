cd3eeffb6762af434555bb1ee2be4588b8b45268
ccatron88 <christophercatron@outlook.com>
2026-04-15 23:11:18 -0500

    Initial push

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+print('hello world')
+
+def main():
+    text_node = TextNode("On the Nature of Daylight", "link", "https://en.wikipedia.org/wiki/On_the_Nature_of_Daylight")
+    print(text_node)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = 'text'
+    BOLD_TEXT = '**Bold text**'
+    ITALIC_TEXT = '_Italic text_'
+    CODE_TEXT = '`Code text`'
+    LINK = '[anchor text](url)'
+    IMAGE = '![alt text](url)'
+
+class TextNode:
+    def __init__(self, text, text_type, url = None): 
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __hash__(self):
+        return hash((self.text, self.text_type, self.url))    
+
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            return NotImplemented
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"{self.text} {self.text_type.value} {self.url}"
