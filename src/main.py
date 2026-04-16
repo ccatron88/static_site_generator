20450be323d074ac60571c5bf560690cd8a4fe3a
ccatron88 <christophercatron@outlook.com>
2026-04-15 23:28:37 -0500

    Add text_node data Added correct values to pass to TextNode class in order to print correctly.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from textnode import TextNode
 print('hello world')
 
 def main():
-    text_node = TextNode("On the Nature of Daylight", "link", "https://en.wikipedia.org/wiki/On_the_Nature_of_Daylight")
+    text_node = TextNode("On the Nature of Daylight", "text", "https://en.wikipedia.org/wiki/On_the_Nature_of_Daylight")
     print(text_node)
 
 main()
