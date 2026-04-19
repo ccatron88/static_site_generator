9f97e0c3541fff1867ed7df15eeadd1f937960c9
ccatron88 <christophercatron@outlook.com>
2026-04-19 17:45:30 -0500

    Edit Tests Added new tests to verify individual properties of the HTMLNode class. Also updated the HTMLNode class to more closely resemble desired output

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 
-class HtmlNode:
+class HTMLNode:
 
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -8,16 +8,15 @@ class HtmlNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError("to_html method not implemented")
     
     def props_to_html(self):
         html_props_string = ''
-        if self.props is None or self.props is '':
+        if self.props is None:
             return ''
-        for (key, value) in self.props:
-            string += f" {key}='{value}' "
+        for prop in self.props:
+            html_props_string += f' {prop}="{self.props[prop]}"'
         return html_props_string
     
     def __repr__(self):
-        node = HtmlNode(self.tag, self.value, self.children, self.props)
-        print(f"{node.tag},\n{node.value},\n{node.children},\n{node.props}")
+        return (f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})")
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,23 +1,45 @@
 import unittest
-from htmlnode import HtmlNode 
+from htmlnode import HTMLNode 
 
-class TestHtmlNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
 
     def test_html_props(self):
-        node_props = {
-            "href": "https://www.google.com",
-            "target": "_blank" 
-        }
-        node = HtmlNode("<a>", "google", None, node_props)
-        self.assertIsNotNone(node.props_to_html)
+        node = HTMLNode("a", "google", None, {"class": "link"})
+        
+        self.assertEqual(
+            node.tag,
+            "a"
+        )
+
+        self.assertEqual(
+            node.value,
+            "google"
+        )
+
+        self.assertEqual(
+            node.children,
+            None
+        )
+
+        self.assertEqual(
+            node.props,
+            {'class': 'link'}
+        )
 
     def test_to_html_props(self):
         node_props = {
             "href": "https://www.google.com",
             "target": "_blank" 
         }
-        node = HtmlNode("<a>", "google", None, node_props)
+        node = HTMLNode("a", "google", None, node_props)
         self.assertEqual(
             node.props_to_html(),
-            " href='https://www.google.com' ",
+            ' href="https://www.google.com" target="_blank"',
+        )
+
+    def test_html_repr(self):
+        node = HTMLNode("a", "harry potter", None, {"class": "redirect", "href": "https://harrypotter.com"})
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(a, harry potter, children: None, {'class': 'redirect', 'href': 'https://harrypotter.com'})"
         )
