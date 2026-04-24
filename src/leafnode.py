875dd6fee42628e446f6405b50a857ddd6f7b10e
ccatron88 <christophercatron@outlook.com>
2026-04-23 22:07:37 -0500

    Minor leafnode.py update Fixed the super() of leafnode to fill in the missing value with a 'None' parameter.

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,7 +2,7 @@ from htmlnode import *
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, props)
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value is None:
