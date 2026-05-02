654d897aac2e275e571a6a336a999b6a5bc3a048
ccatron88 <christophercatron@outlook.com>
2026-05-01 21:41:55 -0500

    Daily update

--- src/parentnode.py
+++ src/parentnode.py
@@ -2,13 +2,13 @@ from htmlnode import *
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        # super().__init__(tag, None, children, props)
+        super().__init__(tag, None, children, props)
 
     def to_html(self):
-        if self.tag is None:
-            raise ValueError('a tag is required')
-        if self.children is None:
-            raise ValueError('a parent node must have children')
+        # if self.tag is None:
+        #     raise ValueError('a tag is required')
+        # if self.children is None:
+        #     raise ValueError('a parent node must have children')
         
         if self.children:
             return (f"<{self.tag}>{self.to_html(self.children)}</{self.tag}>")
