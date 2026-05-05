9fa69fde15d832457b64d530cecc454902945b62
ccatron88 <christophercatron@outlook.com>
2026-05-04 22:25:21 -0500

    Refactor parentnode children logic Refactored parentnode logic regarding child nodes and the format of their return value

--- src/parentnode.py
+++ src/parentnode.py
@@ -1,4 +1,5 @@
 from htmlnode import *
+from leafnode import LeafNode
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
@@ -7,9 +8,14 @@ class ParentNode(HTMLNode):
     def to_html(self):
         if self.tag is None:
             raise ValueError('a tag is required')
-        # if self.children is None:
-        #     raise ValueError('a parent node must have children')
+        if self.children is None:
+            raise ValueError('a parent node must have children')
         
+        full_string = ''
+        child_string = ''
         if self.children:
-            return (f"<{self.tag}>{self.to_html(self.children)}</{self.tag}>")
-        return (f"<{self.tag}>{self.children}</{self.tag}>")
+            for child in self.children:
+                child_string += f"<{child.tag} {child.props}>{child.value}</{child.tag}>"
+            full_string += f"<{self.tag} {self.props}>{child_string}</{self.tag}>"
+            return full_string
+        
