ed751fa55e3b1963ed931a587a8f55ddb39ed1fb
ccatron88 <christophercatron@outlook.com>
2026-05-23 22:54:28 -0500

    Daily update

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -5,19 +5,19 @@ from leafnode import LeafNode
 from htmlnode import HTMLNode
 
 class TestParentNode(unittest.TestCase):
-    def test_to_html_with_children(self):
-        child_node = LeafNode("span", "child")
-        parent_node = ParentNode("div", [child_node])
-        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
-
-    # def test_to_html_with_grandchildren(self):
-    #     grandchild_node = LeafNode("b", "grandchild")
-    #     child_node = ParentNode("span", [grandchild_node])
+    # def test_to_html_with_children(self):
+    #     child_node = LeafNode("span", "child")
     #     parent_node = ParentNode("div", [child_node])
-    #     self.assertEqual(
-    #         parent_node.to_html(),
-    #         "<div><span><b>grandchild</b></span></div>",
-    #     )
+    #     self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
 
     def test_to_html_with_grandchildren_and_props(self):
         grandchild_node = LeafNode("b", "grandchild", {"color": "black"})
