a9aabf9a448f1b2ab53f7222a3c8cf03be6b3f71
ccatron88 <christophercatron@outlook.com>
2026-06-11 21:32:34 -0500

    Daily update

--- src/textnode.py
+++ src/textnode.py
@@ -30,7 +30,7 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
-def text_node_to_html_node(text_node: TextNode) -> LeafNode:
-    if not isinstance(text_node, TextNode):
-        raise TypeError("text_node is not type TextNode")
-    # return LeafNode(text_node.tag, text_node.value, text_node.props).to_html()
+    def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+        if not isinstance(text_node, TextNode):
+            raise TypeError("text_node is not type TextNode")
+        return LeafNode(text_node.tag, text_node.value, text_node.props).to_html()
