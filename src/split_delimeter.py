84f9e62c1015687b92ddf485e00b43668a7ad4bd
ccatron88 <christophercatron@outlook.com>
2026-07-11 23:57:22 -0500

    Daily update

--- src/split_delimeter.py
+++ src/split_delimeter.py
@@ -5,10 +5,18 @@ def split_nodes_delimeter(old_nodes: list[TextNode], delimeter: str, text_type: 
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             split_list.extend(node)
-        if node.count(delimeter) != 2:
+        if node.text.count(delimeter) != 2:
             raise ValueError("Markdown characters require an open and closing delimeter (i.e. **word**)")
-        temp_list = node.split(sep=delimeter, maxsplit=2)
+        temp_list = node.text.split(sep=delimeter, maxsplit=2)
         for item in temp_list:
-            split_list.extend(item, item.TextType)
+            if item[0] is delimeter:
+                new_node = ""
+                if delimeter == "`":
+                    new_node = TextNode(item, TextType.CODE)
+                elif delimeter == "**":
+                    new_node = TextNode(item, TextType.BOLD)
+                elif delimeter == "_":
+                    new_node = TextNode(item, TextType.ITALIC)
+                split_list.extend(new_node, new_node.TextType)
     return split_list
             
