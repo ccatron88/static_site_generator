1b477b9b7f5f36d68dfd0283e92289c228b8100f
ccatron88 <christophercatron@outlook.com>
2026-07-19 22:46:07 -0500

    Daily update

--- src/markdown_extractions.py
+++ src/markdown_extractions.py
@@ -2,5 +2,5 @@ from textnode import TextNode, TextType
 import re
 
 def extract_markdown_images(text):
-    images_re = re.findall(r"\[([A-Za-z]w+)\]", text)
+    images_re = re.findall(r"\[([A-Za-z]w+)\]\(w+\)", text)
     return images_re
