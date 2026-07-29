ea83648f53c183b1469613b21e2b98abacf55cf9
ccatron88 <christophercatron@outlook.com>
2026-07-28 22:48:42 -0500

    Daily update

--- src/split_data.py
+++ src/split_data.py
@@ -25,11 +25,14 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             split_list.append(node)
+        if extract_markdown_images(node):
+            extracted_image = extract_markdown_images(node)
+            split_list.append(node.text.split(extracted_image))
     return split_list
 
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
     split_list = []
     for node in old_nodes:
-    #     if node.text_type != TextType.TEXT:
-    #         split_list.append(node)
+        if node.text_type != TextType.TEXT:
+            split_list.append(node)
         return
