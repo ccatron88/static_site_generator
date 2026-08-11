f12d9f37ff73523b85df29c8db9d1254d6799cda
ccatron88 <christophercatron@outlook.com>
2026-08-10 21:30:22 -0500

    Daily update

--- src/split_data.py
+++ src/split_data.py
@@ -34,7 +34,8 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
     split_list = []
     for node in old_nodes:
-        # if node.text_type != TextType.TEXT:
+        # Need to customize to differentiate from split_nodes_image
+        if node.text_type != TextType.TEXT:
             split_list.append(node)
         if extract_markdown_links(node):
             extracted_link = extract_markdown_links(node)
